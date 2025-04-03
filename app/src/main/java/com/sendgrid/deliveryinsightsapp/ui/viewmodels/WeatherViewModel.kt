735f41a27b9da7aaafe82b4b5e4708964c69dd32
bioladev <biola.dev@gmail.com>
2025-04-03 20:26:21 +0100

    feat: Implement WeatherViewModel with StateFlow for UI state

---
+++ app/src/main/java/com/sendgrid/deliveryinsightsapp/ui/viewmodels/WeatherViewModel.kt
@@ -0,0 +1,64 @@
+package com.sendgrid.deliveryinsightsapp.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sendgrid.deliveryinsightsapp.data.repository.WeatherRepository
+import com.sendgrid.deliveryinsightsapp.domain.models.RouteHistory
+import com.sendgrid.deliveryinsightsapp.domain.models.Weather
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class WeatherViewModel(private val repository: WeatherRepository) : ViewModel() {
+
+    private val _pickup = MutableStateFlow("")
+    val pickup: StateFlow<String> = _pickup
+
+    private val _dropOff = MutableStateFlow("")
+    val dropOff: StateFlow<String> = _dropOff
+
+    private val _pickupWeather = MutableStateFlow<Weather?>(null)
+    val pickupWeather: StateFlow<Weather?> = _pickupWeather
+
+    private val _dropOffWeather = MutableStateFlow<Weather?>(null)
+    val dropOffWeather: StateFlow<Weather?> = _dropOffWeather
+
+    private val _recommendation = MutableStateFlow("")
+    val recommendation: StateFlow<String> = _recommendation
+
+    private val _history = MutableStateFlow<List<RouteHistory>>(emptyList())
+    val history: StateFlow<List<RouteHistory>> = _history
+
+
+    fun onPickupChanged(newPickup: String) {
+        _pickup.value = newPickup
+    }
+
+    fun onDropOffChanged(newDropOff: String) {
+        _dropOff.value = newDropOff
+    }
+
+    fun fetchWeatherData() {
+        if (_pickup.value.isBlank() || _dropOff.value.isBlank()) {
+            return
+        }
+        viewModelScope.launch {
+
+                val pickupWeatherData = repository.fetchWeather(location = pickup.value)
+                val dropOffWeatherData = repository.fetchWeather(dropOff.value)
+
+                _pickupWeather.value = pickupWeatherData
+                _dropOffWeather.value = dropOffWeatherData
+
+                /* TODO Add Recommendation Logic */
+
+                repository.addRouteHistory(RouteHistory(pickup = pickup.value, dropOff = dropOff.value))
+                _history.value = repository.getHistory()
+
+                _pickup.value = ""
+                _dropOff.value = ""
+
+        }
+    }
+
+}
